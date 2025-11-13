60a1522fa5ae8e4340b89aee589980c18e9d371d
Daniel Hatfield <danhat1020@gmail.com>
2025-11-13 15:12:12 +0000

    Added font

--- wezterm.lua
+++ wezterm.lua
@@ -18,18 +18,35 @@ config.enable_tab_bar = false
 config.default_cwd = "/home/slick"
 -- fonts
 local fonts = {
-	"IosevkaTerm",
-	"IosevkaTerm Slab",
+	{
+		name = "IosevkaTerm",
+		width = 1.0,
+		height = 1.0,
+	},
+	{
+		name = "IosevkaTerm Slab",
+		width = 1.0,
+		height = 1.0,
+	},
+	{
+		name = "GoMono",
+		width = 0.9,
+		height = 1.1,
+	},
 }
 local current_font = 1
 wezterm.on("toggle-font", function(window, pane)
 	current_font = (current_font % #fonts) + 1
-	local f = fonts[current_font] .. " Nerd Font"
+	local f = fonts[current_font]
 	window:set_config_overrides({
-		font = wezterm.font(f),
+		font = wezterm.font(f.name .. "Nerd Font"),
+		cell_width = f.width,
+		line_height = f.height,
 	})
 end)
-config.font = wezterm.font(fonts[current_font] .. " Nerd Font")
+config.font = wezterm.font(fonts[current_font].name .. " Nerd Font")
+config.cell_width = fonts[current_font].width
+config.line_height = fonts[current_font].height
 config.font_size = 17
 config.adjust_window_size_when_changing_font_size = false
 config.bold_brightens_ansi_colors = "No"
