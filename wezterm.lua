a7a319255801fa0fff3e0c0a180bd2bb5f5314f2
Daniel Hatfield <danhat1020@gmail.com>
2025-11-13 02:01:42 +0000

    Change font cycle function

--- wezterm.lua
+++ wezterm.lua
@@ -1,9 +1,7 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
-
 -- enable wayland support
 config.enable_wayland = false
-
 -- colors
 config.color_scheme = "Ollie"
 config.colors = {
@@ -14,65 +12,30 @@ config.colors = {
 	cursor_fg = "#080808",
 	cursor_border = "#f0f0f0",
 }
-
 -- disable tab bar
 config.enable_tab_bar = false
-
 -- default directory
 config.default_cwd = "/home/slick"
-
 -- fonts
 local fonts = {
-	{
-		display_name = "Fira Code",
-		name = "FiraCode Nerd Font",
-		size = 16,
-		line_height = 1.1,
-		cell_width = 0.9,
-	},
-	{
-		display_name = "Iosevka Term",
-		name = "IosevkaTerm Nerd Font",
-		size = 17,
-		line_height = 1.0,
-		cell_width = 1.0,
-	},
-	{
-		display_name = "Iosevka Term Slab",
-		name = "IosevkaTerm Slab Nerd Font",
-		size = 17,
-		line_height = 1.0,
-		cell_width = 1.0,
-	},
+	"IosevkaTerm",
+	"IosevkaTerm Slab",
 }
-local current_font = #fonts
+local current_font = 1
 wezterm.on("toggle-font", function(window, pane)
 	current_font = (current_font % #fonts) + 1
-	local f = fonts[current_font]
+	local f = fonts[current_font] .. " Nerd Font"
 	window:set_config_overrides({
-		font = wezterm.font(f.name),
-		font_size = f.size,
-		line_height = f.line_height,
-		cell_width = f.cell_width,
+		font = wezterm.font(f),
 	})
 end)
-
-config.font = wezterm.font(fonts[current_font].name)
-config.font_size = fonts[current_font].size
-config.line_height = fonts[current_font].line_height
-config.cell_width = fonts[current_font].cell_width
-
-config.keys = {
-	{ key = "F", mods = "CTRL|SHIFT", action = wezterm.action.EmitEvent("toggle-font") },
-}
-
+config.font = wezterm.font(fonts[current_font] .. " Nerd Font")
+config.font_size = 17
 config.adjust_window_size_when_changing_font_size = false
 config.bold_brightens_ansi_colors = "No"
 config.freetype_load_target = "Normal"
-
 -- cursor
 config.default_cursor_style = "SteadyBlock"
-
 -- terminal
 config.term = "xterm-256color"
 config.window_close_confirmation = "NeverPrompt"
@@ -82,5 +45,6 @@ config.window_padding = {
 	top = 0,
 	bottom = 0,
 }
-
+-- keymaps
+config.keys = { { key = "F", mods = "CTRL|SHIFT", action = wezterm.action.EmitEvent("toggle-font") } }
 return config
