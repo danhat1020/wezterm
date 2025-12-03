86391a06a674d146e37bbaa9a4152ece539d4187
Dan Hatfield <danhat1020@gmail.com>
2025-12-03 15:37:58 +0000

    Updated font

--- wezterm.lua
+++ wezterm.lua
@@ -17,49 +17,24 @@ config.enable_tab_bar = false
 -- default directory
 config.default_cwd = "/home/slick"
 -- fonts
-local fonts = {
-	{
-		name = "JetBrains Mono",
-		size = 16,
-		width = 0.9,
-		height = 1.0,
-	},
-	{
-		name = "JetBrains Mono",
-		size = 20,
-		width = 0.9,
-		height = 1.0,
-	},
-}
-local current_font = 1
-wezterm.on("toggle-font", function(window, pane)
-	current_font = (current_font % #fonts) + 1
-	local f = fonts[current_font]
-	window:set_config_overrides({
-		font = wezterm.font(f.name .. "Nerd Font Mono"),
-		font_size = f.size,
-		cell_width = f.width,
-		line_height = f.height,
-	})
-end)
-config.font = wezterm.font(fonts[current_font].name .. " Nerd Font Mono")
-config.cell_width = fonts[current_font].width
-config.line_height = fonts[current_font].height
-config.font_size = fonts[current_font].size
+config.font = wezterm.font("Iosevka Nerd Font Mono")
+config.font_size = 19
+config.cell_width = 0.9
+config.line_height = 0.9
 config.adjust_window_size_when_changing_font_size = false
 config.bold_brightens_ansi_colors = "No"
 config.freetype_load_target = "Normal"
+config.harfbuzz_features = { "calt=0" }
 -- cursor
 config.default_cursor_style = "SteadyBlock"
 -- terminal
 config.term = "xterm-256color"
 config.window_close_confirmation = "NeverPrompt"
+config.max_fps = 120
 config.window_padding = {
 	left = 0,
 	right = 0,
 	top = 0,
 	bottom = 0,
 }
--- keymaps
-config.keys = { { key = "F", mods = "CTRL|SHIFT", action = wezterm.action.EmitEvent("toggle-font") } }
 return config
